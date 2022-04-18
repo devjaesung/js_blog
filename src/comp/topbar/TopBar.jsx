e1935ce2cb42af96ccfcf42146b4ab19add293e1
devjaesung <jaesungko.dev@gmail.com>
2022-04-18 22:28:51 +0900

    Make Blog website with React.

---
+++ src/comp/topbar/TopBar.jsx
@@ -0,0 +1,40 @@
+import './topbar.css'
+import { Link } from 'react-router-dom';
+
+export default function TopBar() {
+ const user = false;  
+ return (
+    <div className="top">
+        <div className="container topInn">
+        <div className="topLeft">
+             <i className="topIcon fa-brands fa-facebook-square"></i>
+             <i className="topIcon fa-brands fa-twitter-square"></i>
+             <i className="topIcon fa-brands fa-pinterest-square"></i>
+             <i className="topIcon fa-brands fa-instagram-square"></i>
+        </div>
+        <div className="topCenter">
+            <ul className="topList">
+                <li className="topListItem"><Link to="/" className="homeLink">list</Link></li>
+                <li className="topListItem"><a href="#!">about</a></li>
+                <li className="topListItem"><a href="#!">contact</a></li>
+                <li className="topListItem"><Link to ="/Write">write</Link></li>
+                <li className="topListItem">
+                   {
+                       user? ("LOGOUT") : 
+                       (<span className='row'>
+                           <Link to="/Login">LOGIN</Link>
+                           <span> / </span>
+                           <Link to="/Settings">회원가입</Link>
+                       </span>)
+                   }    
+                </li>
+            </ul>
+        </div>
+        <div className="topRight">
+            <img src="images/me.png" alt="홍길동" className="topImg" />
+            <i className="topSearchIcon fas fa-search"></i>
+        </div>
+        </div>
+    </div>
+  )
+}
